1763a64e906f74d4999b40346dae1a77a815bb7c
OussamaBoujdig <oussamaboujdig8@gmail.com>
2025-12-23 22:56:17 +0100

    Add files via upload

---
+++ microservices-messaging-consumer/microservices-messaging-consumer/src/main/java/com/example/microservices_messaging_consumer/MicroMessagingConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices_messaging_consumer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMessagingConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroMessagingConsumerApplication.class, args);
+    }
+}
